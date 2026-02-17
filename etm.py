acbffc5b2b5c32e2f6f0f8e6faa96bab6afe62eb
Stouve <oxide28@gmail.com>
2026-02-17 13:36:32 +0100

    Add Basics functions

--- etm.py
+++ etm.py
@@ -9,6 +9,16 @@ def load_tasks():
     except FileNotFoundError:
         return []
 
+def list_tasks():
+    try:
+        with open("tasks.json", "r") as f:
+            tasks=json.load(f)
+            for element in tasks:
+                print(f"Task : {element["title"]}, DONE") if element["done"] is True else print(f"Task : {element["title"]}, TODO")
+
+    except FileNotFoundError:
+        print("Tasks file not found.")
+
 
 def save_tasks(tasks):
     with open("tasks.json", "w") as f:
@@ -20,9 +30,30 @@ def add_task(title):
     tasks.append({"title": title, "done": False})
     save_tasks(tasks)
 
+def end_task(pos):
+    tasks = load_tasks()
+    tasks[int(pos)-1]["done"] = True
+    save_tasks(tasks)
+
+def remove_task(pos):
+
+    try:
+        tasks = load_tasks()
+        print(f"Deleting task: {tasks[int(pos)-1]['title']} ")
+        tasks.pop(int(pos)-1)
+        save_tasks(tasks)
+
+    except IndexError:
+        print(f"Error Task number {pos} not found. Please check tasks.json file")
 
 if __name__ == "__main__":
     command = sys.argv[1]
 
     if command == "add":
         add_task(sys.argv[2])
+    elif command == "list":
+        list_tasks()
+    elif command == "remove":
+        remove_task(sys.argv[2])
+    elif command == "done":
+        end_task(sys.argv[2])
