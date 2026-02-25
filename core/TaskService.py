fe9ef2c1aa2aa22cc1c2a71b126466e1e111ef18
Stouve <oxide28@gmail.com>
2026-02-25 18:15:45 +0100

    Create TaskService

--- core/TaskService.py
+++ core/TaskService.py
@@ -10,113 +10,19 @@ class TaskManager():
     Handle operations and tasks persistence
     """
 
-    def __init__(self, file_path: str = "tasks.json"):
-        self.file_path = Path(file_path)
-        self.tasks: List[Task] = []
-        self._load_tasks()
+    def __init__(self, repository):
+        self.repository = repository
 
+    def createTask(self, title: str, description : str | None):
+        if not title:
+            raise ValueError("Task title cannot be empty")
+        return self.repository.add(title, description)
 
-    def _load_tasks(self)->None:
-        """
-        Load tasks from json file<
-        """
-        if not self.file_path.exists():
-            self.tasks = []
-            return
-        try:
-            with open(self.file_path, "r", encoding="utf-8") as f:
-                data = json.load(f)
-                #print(f"TEST DATA : {data}")
-                self.tasks = [Task.from_dict(item) for item in data]
-        except json.decoder.JSONDecodeError:
-            self.tasks = []
+    def listTasks(self):
+        return self.repository.get_all_tasks()
 
-    def _save_tasks(self)->None:
-        """
-        Save tasks to JSON file
-        """
-        with open(self.file_path, "w", encoding="utf-8") as f:
-            json.dump(
-                [task.to_dict() for task in self.tasks],
-                f,
-                indent=4,
-                ensure_ascii=False
-            )
+    def completeTask(self, task_id: int):
+        return self.repository.mark_done(task_id)
 
-    def add_task(self, title)->"Task":
-        """
-        Add a new task
-        """
-        task_id = self._generate_next_id()
-        task = Task(task_id, title)
-        self.tasks.append(task)
-        self._save_tasks()
-        return task
-
-    def list_tasks(self)->List[Task]:
-        """
-        return all tasks
-        """
-        return self.tasks
-
-
-    def gest_task(self,task_id:int)->Optional[Task]:
-        """
-        Return task by ID
-        """
-        for task in self.tasks:
-            if task.id == task_id:
-                return task
-
-        return  None
-
-    def complete_task(self,task_id:int)->bool:
-        """
-        Complete task
-        """
-        task=self.gest_task(task_id)
-
-        if not task:
-            return False
-
-        task.mark_done()
-        self._save_tasks()
-        return True
-
-    def reset_task(self,task_id:int)->bool:
-        """
-        Complete task
-        """
-        task=self.gest_task(task_id)
-
-        if not task:
-            return False
-
-        task.mark_undone()
-        self._save_tasks()
-        return True
-
-    def delete_task(self, task_id:int)->bool:
-        """
-        Delete task by ID
-        """
-        task=self.gest_task(task_id)
-
-        if not task:
-            return False
-
-        self.tasks.remove(task)
-        self._save_tasks()
-        return True
-
-        print(f"Deleting Task : {self.tasks[pos]}")
-
-    def _generate_next_id(self)-> int:
-        """
-        Generate next task ID
-        """
-        if not self.tasks:
-            return 1
-
-        return max(task.id for task in self.tasks) + 1
-
+    def deleteTask(self, task_id: int):
+        return self.repository.delete(task_id)
