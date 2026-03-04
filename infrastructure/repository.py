cc67432f9061273174ea11ce7ffda0660a630b9f
Stouve <oxide28@gmail.com>
2026-03-04 16:16:04 +0100

    create SQL requests

--- infrastructure/repository.py
+++ infrastructure/repository.py
@@ -1,6 +1,42 @@
 from infrastructure.database import get_connection
+from core.models import Task
 
 
-#class SQLiteTaskRepository:
+class SQLiteTaskRepository:
 
-    #def add(self,title:str, description:str | None):
+    def add(self,title:str, description:str):
+
+        conn=get_connection()
+        cursor=conn.cursor()
+
+        cursor.execute("INSERT INTO tasks (title,description) VALUES (?,?)",
+                       (title,description)
+                       )
+        conn.commit()
+        conn.close()
+
+     def get_all_tasks(self):
+
+         conn=get_connection()
+         cursor=conn.cursor()
+         cursor.execute("SELECT * FROM tasks")
+         tasks=cursor.fetchall()
+         conn.close()
+
+         return tasks
+
+    def mark_done(self,task_id):
+
+        conn=get_connection()
+        cursor=conn.cursor()
+        cursor.execute("UPDATE tasks SET status = 'done' WHERE id=?",
+                       (task_id,))
+        conn.commit()
+        conn.close()
+
+def delete(self,task_id):
+        conn=get_connection()
+        cursor=conn.cursor()
+        cursor.execute("DELETE FROM tasks WHERE id=?",(task_id,))
+        conn.commit()
+        conn.close()
