7cc6dfb2071a242a46bcc73a3b31fc40b8cd7a1f
Stouve <oxide28@gmail.com>
2026-03-12 17:27:17 +0100

    create add task test function

--- tests/test_task_service.py
+++ tests/test_task_service.py
@@ -2,9 +2,23 @@ import pytest
 
 from core.services import TaskService, InvalidTaskError
 from core.models import Task, TaskStatus
-from datetime import datetime
+from datetime import datetime, timezone
 
 class FakeRepository:
     def __init__(self):
         self.tasks = {}
         self.counter = 1
+
+    def add(self, title, description)->Task:
+        now = datetime.now(timezone.utc).isoformat()
+        task = Task(
+            title=title,
+            description=description,
+            status=TaskStatus.PENDING,
+            created_at=datetime.fromisoformat(now)
+        )
+        self.tasks[self.counter] = task
+        self.counter += 1
+        return task
+
+
