dd039994ae3a7f1f4ba686564e8d54fe2b804d48
Stouve <oxide28@gmail.com>
2026-03-19 16:01:16 +0100

    Add test with temporary DB

---
+++ tests/test_repository_sqlite.py
@@ -0,0 +1,37 @@
+from numpy.f2py.symbolic import as_eq
+
+from core.models import TaskStatus
+
+def test_add_and_get(repo):
+    task = repo.add("Test",None)
+
+    tasks=repo.get_all_tasks
+
+    assert len(tasks) == 1
+    assert tasks[0].title == "Test"
+
+def test_get_by_id(repo):
+    task = repo.add("Test",None)
+
+    result=repo.get_by_id(task.id)
+    assert result is not None
+    assert result.id == task.id
+
+def mark_done(repo):
+    task = repo.add("Test",None)
+
+    repo.mark_done(task.id)
+
+    updated_task = repo.get_by_id(task.id)
+
+    assert updated_task.status == TaskStatus.DONE
+
+def test_delete(repo):
+    task = repo.add("Test",None)
+
+    repo.delete(task.id)
+
+    tasks=repo.get_all_tasks()
+
+    assert len(tasks) == 0
+
