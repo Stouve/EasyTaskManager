c1ba96d179c6ee6634784c36b51f3aae3be6e6ce
Stouve <oxide28@gmail.com>
2026-04-02 15:02:58 +0200

    Create correct fixtures with correct scope

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -1,6 +1,35 @@
-from app.infrastructure.database import SessionLocal
+import pytest
+from sqlalchemy import create_engine
+
+from sqlalchemy.orm import Session
+from app.infrastructure.database import SessionLocal, Base, engine
 from app.infrastructure.repository import TaskRepository
 from app.core.task import TaskStatus
+
+# Engine once
+@pytest.fixture(scope="session")
+def engine():
+    return create_engine("sqlite:///:memory:")
+
+#Tables once
+@pytest.fixture(scope="session", autouse=True)
+def create_tables(engine):
+    Base.metadata.create_all(engine)
+    yield
+    Base.metadata.drop_all(engine)
+
+#Session, one per test
+@pytest.fixture(scope="function")
+def db_session(engine):
+    connection = engine.connect()
+    transaction = connection.begin()
+
+    session = Session(bind=engine)
+    yield session
+
+    session.close()
+    transaction.rollback()
+    connection.close()
 
 
 def test_create_task():
