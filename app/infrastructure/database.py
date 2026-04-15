1074fcd5fe918eb893b3ab3ab7549cb18bc09b4b
Stouve <oxide28@gmail.com>
2026-04-15 17:16:08 +0200

    Create function to get db

--- app/infrastructure/database.py
+++ app/infrastructure/database.py
@@ -7,7 +7,16 @@ engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
+#Create configured session class
 SessionLocal = sessionmaker(bind=engine)
 
+#Create base class for all models
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
