96e39722026a33aed5b56a1921841b422026f60d
Stouve <oxide28@gmail.com>
2026-04-22 16:28:51 +0200

    Fix error in router + change DB name

--- app/routers/task_router.py
+++ app/routers/task_router.py
@@ -2,8 +2,6 @@ from fastapi import APIRouter, Depends
 from app.infrastructure.database import get_db
 from app.infrastructure.repository import TaskRepository
 
-def get_task_repository():
-
 router = APIRouter(
     prefix="/tasks",
     tags=["Tasks"]
@@ -15,3 +13,4 @@ def get_task_repository(db=Depends(get_db)):
 @router.get("/")
 def get_tasks(repo : TaskRepository = Depends(get_task_repository)):
     return repo.get_all_tasks()
+
