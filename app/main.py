d9a74265c32c9a7c5c5664f5ee79c16d480e069e
Stouve <oxide28@gmail.com>
2026-04-22 16:30:25 +0200

    Add create tasks table at startup

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
+
 from app.routers.task_router import router as task_router
+from app.infrastructure.database import Base, engine
+from app.infrastructure.db_models import TaskModel
+
+#Create table at startup
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(task_router)
