ca42f46a8bf85dc2916be71ca101887baf6a7b57
Stouve <oxide28@gmail.com>
2026-04-26 18:38:14 +0200

    finish task_schema

---
+++ app/schemas/task_schema.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+from typing import Optional
+
+from app.core.task import TaskStatus
+
+
+#For DATA IN (POST/PUT)
+class TaskCreate(BaseModel):
+    title:str
+    description: Optional[str] = None
+
+
+#For DATA OUT (GET)
+class TaskOut(BaseModel):
+    id: int
+    title: str
+    status: TaskStatus
+    created_at: datetime
+    description: Optional[str] = None
+
+    class Config:
+        orm_mode = True
