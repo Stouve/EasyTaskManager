7b6e4435a1b89ddc96d769723d9dfd725253e341
Stouve <oxide28@gmail.com>
2026-04-27 16:27:45 +0200

    Add service into router

--- app/routers/task_router.py
+++ app/routers/task_router.py
@@ -1,25 +1,30 @@
 from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
 from app.core import task
 from app.infrastructure.database import get_db
 from app.infrastructure.repository import TaskRepository
 from app.schemas.task_schema import TaskOut, TaskCreate
 from typing import List
+from app.core.services import Task, TaskService
 
 router = APIRouter(
     prefix="/tasks",
     tags=["Tasks"]
 )
 
-def get_task_repository(db=Depends(get_db)):
+def get_task_repository(db : Session = Depends(get_db)):
     return TaskRepository(db)
 
+def get_task_service(repo : TaskRepository = Depends(get_task_repository)):
+    return TaskService(repo)
+
 @router.get("/", response_model=List[TaskOut])
-def get_tasks(repo : TaskRepository = Depends(get_task_repository)):
-    return repo.get_all_tasks()
+def get_tasks(service : TaskService = Depends(get_task_service)):
+    return service.list_tasks()
 
 @router.post("/", response_model=TaskOut)
 def create_task(task: TaskCreate,
-                repo : TaskRepository = Depends(get_task_repository)
+                service : TaskService = Depends(get_task_service)
                 ):
-    return repo.add(task.title, task.description)
+    return service.create_task(task.title, task.description)
