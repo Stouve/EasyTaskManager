7ff97b5a0f73503405c658d5d3ff72f405f27e46
Stouve <oxide28@gmail.com>
2026-06-10 17:01:03 +0200

    Modify database conf for postgresql

--- app/infrastructure/database.py
+++ app/infrastructure/database.py
@@ -5,11 +5,9 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL","sqlite:///./tasks.db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 #Create configured session class
 SessionLocal = sessionmaker(bind=engine)
