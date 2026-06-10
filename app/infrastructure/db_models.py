d111658e53a8df915e4ccc116088201c40fde71b
Stouve <oxide28@gmail.com>
2026-06-10 17:02:44 +0200

    Modify task tabke conf for postgresql

--- app/infrastructure/db_models.py
+++ app/infrastructure/db_models.py
@@ -1,27 +1,26 @@
 from sqlalchemy import Column, Integer, String, DateTime, Enum
 from datetime import datetime, timezone
 from app.infrastructure.database import Base
-from app.core.task import TaskStatus  # On utilise l'enum métier
+from app.core.task import TaskStatus
 
 class TaskModel(Base):
     """
     Modèle SQLAlchemy représentant la table 'tasks'
     """
-
     __tablename__ = "tasks"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False, index=True)
-    description = Column(String, nullable=True)
+    title = Column(String(255), nullable=False, index=True)
+    description = Column(String(1000), nullable=True)
 
     status = Column(
-        Enum(TaskStatus),
+        Enum(TaskStatus, name="taskstatus"),
         default=TaskStatus.PENDING,
         nullable=False
     )
 
     created_at = Column(
-        DateTime,
+        DateTime(timezone=True),
         default=lambda: datetime.now(timezone.utc),
         nullable=False
     )
