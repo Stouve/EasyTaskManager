b3430553288fec3cb6e84d672fee268801867cfe
Stouve <oxide28@gmail.com>
2026-06-14 18:18:34 +0200

    Add exception for get_tasks in router

--- app/routers/task_router.py
+++ app/routers/task_router.py
@@ -39,7 +39,11 @@ def create_task(task: TaskCreate,
 
 @router.get("/{task_id}", response_model=TaskOut)
 def get_task_by_id(task_id: int, service : TaskService = Depends(get_task_service)):
-    return service.get_task(task_id)
+
+    try:
+        return service.get_task(task_id)
+    except TaskNotFoundError:
+        raise HTTPException(status_code=404, detail="Task not found")
 
 @router.put("/{task_id}", response_model=TaskOut)
 def update_task(task_id: int,
