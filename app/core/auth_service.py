e71cabdcc534f94e84cfd76cd914bd2376ef35da
Stouve <oxide28@gmail.com>
2026-06-28 16:03:14 +0200

    Add register fct to auth_service

--- app/core/auth_service.py
+++ app/core/auth_service.py
@@ -1,6 +1,9 @@
 from datetime import datetime, timezone
 
+from sqlalchemy.sql.functions import user
+
 from app.core.user import User, RoleEnum
+from app.infrastructure.user_repository import UserRepository
 from app.security.password_hasher import hash_password, verify_password
 from app.security.jwt_handler import (
     create_access_token,
@@ -29,3 +32,18 @@ class InactiveUserError(AuthError):
 class InvalidRefreshToken(AuthError):
     """Raised when refresh token is invalid, expired, revoked, or unknown"""
 
+class AuthService:
+
+    def __init__(self, user_repository: UserRepository) -> User:
+        self.user_repository = user_repository
+
+    def register(self,email:str, password:str)->User:
+        email=email.strip().lower()
+
+        existing = self.user_repository.get_by_email(email)
+        if existing is not None:
+            raise EmailAlreadyExists("Email already exists")
+
+        hashed_password = hash_password(password)
+
+        return(self.user_repository.add(email, hashed_password, role=RoleEnum.USER))
