21de4e007e142196bcd5129a495b9993a20a4e17
Stouve <oxide28@gmail.com>
2026-07-28 18:21:08 +0200

    Add auth_router & imports to main

--- app/main.py
+++ app/main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
 
 from app.routers.task_router import router as task_router
+from app.routers.task_router import router as auth_router
 from app.infrastructure.database import Base, engine
 from app.infrastructure.db_models import TaskModel
+from app.infrastructure.user_models import UserModel, RefreshTokenModel
 
 #Create table at startup
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(task_router)
+app.include_router(auth_router)
 
 @app.get("/")
 def root():
