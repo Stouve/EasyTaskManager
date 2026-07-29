bbc2ea33f0ef5c1dbbb85e49bd41d632f449aa39
Stouve <oxide28@gmail.com>
2026-07-29 10:38:48 +0200

    Fix model imports in main

--- app/main.py
+++ app/main.py
@@ -3,8 +3,7 @@ from fastapi import FastAPI
 from app.routers.task_router import router as task_router
 from app.routers.task_router import router as auth_router
 from app.infrastructure.database import Base, engine
-from app.infrastructure.db_models import TaskModel
-from app.infrastructure.user_models import UserModel, RefreshTokenModel
+from app.infrastructure.models import UserModel, RefreshTokenModel, TaskModel
 
 #Create table at startup
 #This line is redundant due to Alembic
