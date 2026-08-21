41f29c738b176cd26c41d9663031f6020fc6f871
Stouve <oxide28@gmail.com>
2026-08-21 10:25:44 +0200

    add 4 tests in test_auth_service

--- tests/test_auth_service.py
+++ tests/test_auth_service.py
@@ -1,8 +1,8 @@
 import pytest
 
-from app.infrastructure.user_repository import UserRepository
 from app.security.password_hasher import verify_password
 from app.core.user import RoleEnum
+from app.core.auth_service import EmailAlreadyExistsError, InvalidCredentialsError, InactiveUserError
 
 def test_register_creates_user(auth_service):
 
@@ -17,7 +17,32 @@ def test_register_creates_user(auth_service):
     assert user_created.id is not None
     assert user_created.email==email
     assert verify_password(password, user_created.hashed_password) is True
+    assert user_created.hashed_password != password #check that password is hashed and not plain
     assert user_created.role==RoleEnum.USER
     assert user_created.is_active is True
-    assert user_created.hashed_password != password
 
+
+def test_register_with_existing_email_raises_error(auth_service):
+
+    email = "test@example.com"
+    password = "strongpassword123"
+    user_created=auth_service.register(email,password)
+
+    with pytest.raises(EmailAlreadyExistsError):
+        user_created = auth_service.register(email, "anotherpassword456")
+
+def test_authenticate_with_wrong_password_raises_error(auth_service):
+    email = "test@example.com"
+    password = "strongpassword123"
+    user_created = auth_service.register(email, password)
+
+    with pytest.raises(InvalidCredentialsError):
+        user=auth_service.authenticate(email, "wrong_password")
+
+def test_authenticate_with_wrong_user_raises_error(auth_service):
+    email = "test@example.com"
+    password = "strongpassword123"
+    user_created = auth_service.register(email, password)
+
+    with pytest.raises(InvalidCredentialsError):
+        user=auth_service.authenticate("fake@example.com", password)
